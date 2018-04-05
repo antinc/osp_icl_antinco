c23e093e4ec7114160579d60643e89263f4b8bb1
antinc <35619278+antinc@users.noreply.github.com>
2018-04-05 02:44:17 -0700

    Add files via upload
    Here is an updated version of icl1.c, with a horrid attempt at file I/O

--- icl1.c
+++ icl1.c
@@ -2,5 +2,11 @@
 int main()
 {
 printf("Hello World!");
+FILE *fopen( const char * /tmp/<antinc>.txt, const char * mode );
+int fputc( string "I feel so inspired! Ha ha!", FILE *fp );
+
+
+
+
 return0;
 }
